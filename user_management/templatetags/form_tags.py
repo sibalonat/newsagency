8814c2c15e22a0ee04c6da9f545ad091eadd32bd
Marin Nikolli <sibalonat@gmail.com>
2024-12-04 00:05:28 +0100

    fix: create template for users

--- user_management/templatetags/form_tags.py
+++ user_management/templatetags/form_tags.py
@@ -6,11 +6,13 @@ register = template.Library()
 def add_class(field, css_class):
     return field.as_widget(attrs={"class": css_class})
 
-# @register.filter(name='add_attrs')
-# def add_attrs(field, css_class):
-#     attrs = {}
-#     definitions = css_class.split(',')
-#     for definition in definitions:
-#         key, value = definition.split(':')
-#         attrs[key.strip()] = value.strip()
-#     return field.as_widget(attrs=attrs)
+@register.filter(name='add_attrs')
+def add_attrs(field, attrs):
+    if not hasattr(field, 'as_widget'):
+        return field
+    attrs_dict = {}
+    definitions = attrs.split(',')
+    for definition in definitions:
+        key, value = definition.split(':')
+        attrs_dict[key.strip()] = value.strip()
+    return field.as_widget(attrs=attrs_dict)
