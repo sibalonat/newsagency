d6b599e2fef4487cd26407c2a09e1c2e685bb71b
Marin Nikolli <sibalonat@gmail.com>
2024-12-04 23:19:12 +0100

    fix: create user works

--- user_management/forms.py
+++ user_management/forms.py
@@ -7,3 +7,9 @@ class UserCreationForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'is_editor', 'is_reader']
+        
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        if len(password) < 8:
+            raise forms.ValidationError("Password must be at least 8 characters long.")
+        return password
