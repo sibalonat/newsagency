e8128026a58a3d298c86059c137f2b56bb8efdea
Marin Nikolli <sibalonat@gmail.com>
2024-12-09 00:35:44 +0100

    fix: filtering articles

--- user_management/static/user_management/index.js
+++ user_management/static/user_management/index.js
@@ -9,18 +9,34 @@ function toggleRoles() {
 }
 
 function getUserArticles(userId) {
-    const userId = document.getElementById('user_id').innerText;
-    const url = "{% url 'user_articles' 0 %}".replace('0', userId);
-    fetch(`/user/${userId}/articles/`)
+    const url = userArticlesUrl.replace('0', userId);    
+    fetch(url)
         .then(response => response.json())
         .then(articles => {
             const container = document.getElementById('articles-container');
             container.innerHTML = '<h3>Articles</h3>';
             if (articles.length > 0) {
                 const list = document.createElement('ul');
+                list.classList.add('list-group-item');
                 articles.forEach(article => {
                     const listItem = document.createElement('li');
-                    listItem.textContent = `${article.title} (Created at: ${article.created_at})`;
+                    listItem.classList.add('list-group-item');
+
+                    const link = document.createElement('a');
+                    link.href = `/article/${article.id}/`;
+                    link.classList.add('text-decoration-none', 'text-dark');
+
+                    const titleParagraph = document.createElement('p');
+                    titleParagraph.classList.add('mb-0');
+                    titleParagraph.textContent = article.title;
+                    link.appendChild(titleParagraph);
+
+                    const dateParagraph = document.createElement('p');
+                    dateParagraph.textContent = `Created at: ${article.created_at}`;
+                    dateParagraph.classList.add('text-sm', 'text-muted', 'mb-0');
+                    link.appendChild(dateParagraph);
+                    
+                    listItem.appendChild(link);
                     list.appendChild(listItem);
                 });
                 container.appendChild(list);
@@ -29,23 +45,3 @@ function getUserArticles(userId) {
             }
         });
 }
-// document.getElementById('username').addEventListener('click', function() {
-//     const userId = {{ user.id }};
-//     fetch(`/user/${userId}/articles/`)
-//         .then(response => response.json())
-//         .then(articles => {
-//             const container = document.getElementById('articles-container');
-//             container.innerHTML = '<h3>Articles</h3>';
-//             if (articles.length > 0) {
-//                 const list = document.createElement('ul');
-//                 articles.forEach(article => {
-//                     const listItem = document.createElement('li');
-//                     listItem.textContent = `${article.title} (Created at: ${article.created_at})`;
-//                     list.appendChild(listItem);
-//                 });
-//                 container.appendChild(list);
-//             } else {
-//                 container.innerHTML += '<p>No articles found.</p>';
-//             }
-//         });
-// });
