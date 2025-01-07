504c7624498a4e58bc03e7141349cdbc3d64c8b0
Marin Nikolli <sibalonat@gmail.com>
2025-01-07 09:21:46 +0100

    fix: remove comments

--- user_management/urls.py
+++ user_management/urls.py
@@ -2,12 +2,10 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LogoutView
 from . import views
-# from .views import CustomLogoutView
 
 app_name = 'management'
 
 urlpatterns = [
-    # path('news/', include('news.urls')),
     path('users/', views.user_list, name='user_list'),
     path('users/<int:user_id>/', views.user_detail, name='user_detail'),
     path('login/', views.custom_login, name='login'),
